37ad5243240a6b9a6c8c5c8de555bbf5c53873e5
colingreens <colin.j.benson@gmail.com>
2023-05-04 13:47:30 -0700

    implement random.choice

--- day-5/password_generator.py
+++ day-5/password_generator.py
@@ -13,18 +13,18 @@ symbols_amount = int(input("How many symbols would you like?\n"))
 password = []
 
 for letter in range(1,letters_amount + 1):
-    password.append(letters[random.randint(0, len(letters))])
+    password.append(random.choice(letters))
 
 for number in range(1,numbers_amount + 1):
     password.append(str(random.randint(0, 9)))
 
 for symbol in range(1,symbols_amount + 1):
-    password.append(symbols[random.randint(0, len(symbols))])
+    password.append(random.choice(symbols))
 
 random.shuffle(password)
 
 passwordString = ""
 for char in password:
     passwordString += char
-    
+
 print(str(passwordString))
