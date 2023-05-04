1f6ac36890133254951a29f32e1c7a2accdb6455
colin_greens <colin.j.benson@gmail.com>
2023-05-03 22:20:38 -0700

    actual end to day 4!

---
+++ day-4/rock_paper_scissors.py
@@ -0,0 +1,26 @@
+import random
+
+player_choice = int(input("What do you choose? 0 for Rock, 1 for Paper, 2 for Scissors. "))
+computer_choice = random.randint(0,2)
+
+rock = "🪨"
+paper = "📃"
+scissors = "✂️"
+
+options_list = [rock, paper, scissors]
+
+print(f"Player throws {options_list[player_choice]}\n")
+print(f"Computer throws {options_list[computer_choice]}")
+
+if player_choice == computer_choice:
+    print("Draw! Throw again")
+elif player_choice == 1 and computer_choice == 2:
+    print("Computer Wins!")
+elif player_choice == 2 and computer_choice == 3:
+    print("Computer Wins!")
+elif player_choice == 3 and computer_choice == 1:
+    print("Computer Wins!")
+else:
+    print("Player wins!")
+
+
