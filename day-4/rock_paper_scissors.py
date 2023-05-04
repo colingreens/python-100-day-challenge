9c4b82f88fdd5d7bd60e5670fca596f4f080a931
colin_greens <colin.j.benson@gmail.com>
2023-05-03 22:21:51 -0700

    small cleanup

--- day-4/rock_paper_scissors.py
+++ day-4/rock_paper_scissors.py
@@ -1,6 +1,6 @@
 import random
 
-player_choice = int(input("What do you choose? 0 for Rock, 1 for Paper, 2 for Scissors. "))
+player_choice = int(input("What do you choose? 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
 computer_choice = random.randint(0,2)
 
 rock = "🪨"
